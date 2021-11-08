2148bc487c8a8ecb45982284656cd3318445cc00
LorenzoDelCerro <49516101+LorenzoDelCerro@users.noreply.github.com>
2021-11-08 19:50:49 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+//Ajoutez ici les eventuels iclude dont vous aurez besoin
+
+int main() {
+    printf("=====\tTPX : XXXXX\t=====\n");
+    int choixExercice = 0;
+
+    //On demande son choix à l'utilisateur
+    printf("Quel exercice voulez-vous lancer ?\n");
+    //Pensez à afficher les choix disponibles ici
+
+    do{
+        scanf("%d", &choixExercice);
+        switch (choixExercice) {
+            case 1 :
+                exercice1();
+                break;
+            case 2 :
+                exercice2();
+                break;
+            case 3 :
+                exercice3();
+                break;
+            case 0 :
+                break;
+            default : {
+                printf("Choix Invalide ! Veuillez selectionner un exercice :\n");
+            
+                break;
+            }
+        }
+    }while (choixExercice != 0); //Si choix == 0 on arrête !
+
+    printf("Au revoir !\n");
+
+    return 0;
+}
